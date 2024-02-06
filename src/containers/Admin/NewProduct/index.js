65ef16364dbc070e2f1cd02680148339c89508a3
Luan <luanitachi@hotmail.com>
2024-02-06 04:20:22 -0300

    att

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -1,40 +1,70 @@
+import UploadFileIcon from '@mui/icons-material/UploadFile'
 import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, Controller } from 'react-hook-form'
 import ReactSelect from 'react-select'
 
-import { Button } from '../../../components'
 import apiCodeburger from '../../../services/api'
-import { Container, Label, Input } from './styles'
+import { ButtonStyle, Container, Label, Input, LabelUpload } from './styles'
 
 function NewProduct() {
-  const { register, handleSubmit } = useForm()
+  const [filename, setFileName] = useState(null)
+  const [categories, setCategores] = useState([])
+  const { register, handleSubmit, control } = useForm()
   const onSubmit = data => {
     console.log(data)
   }
 
   useEffect(() => {
-    async function loadOrders() {
-      const { data } = await apiCodeburger.get('products')
+    async function loadCategories() {
+      const { data } = await apiCodeburger.get('categories')
+
+      setCategores(data)
     }
 
-    loadOrders()
+    loadCategories()
   }, [])
 
   return (
     <Container>
-      <form noValidate>
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
         <Label>Nome</Label>
         <Input type="text" {...register('name')} />
 
         <Label>Preço</Label>
         <Input type="number" {...register('price')} />
 
-        <Label>Upload da imagem</Label>
-        <Input type="file" accept="image/png, image/jpeg" />
+        <LabelUpload>
+          {filename || (
+            <>
+              <UploadFileIcon />
+              Carregue a imagem do produto
+            </>
+          )}
 
-        <ReactSelect />
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            {...register('fle')}
+            onChange={value => setFileName(value.target.files[0]?.name)}
+          />
+        </LabelUpload>
+        <Controller
+          name="category_id"
+          control={control}
+          render={({ field }) => {
+            return (
+              <ReactSelect
+                {...field}
+                options={categories}
+                getOptionLabel={categorie => categorie.name}
+                getOptionValue={categorie => categorie.id}
+                placeholder="Categorias"
+              />
+            )
+          }}
+        ></Controller>
 
-        <Button>Adicionar Produto</Button>
+        <ButtonStyle>Adicionar Produto</ButtonStyle>
       </form>
     </Container>
   )
--- src/containers/Admin/NewProduct/styles.js
+++ src/containers/Admin/NewProduct/styles.js
@@ -1,12 +1,58 @@
 import styled from 'styled-components'
 
+import { Button } from '../../../components'
+
 export const Container = styled.div`
   height: 100%;
   width: 100%;
-  background-color: #d3d3d3;
+  display: flex;
+  justify-content: center;
   align-items: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    background: #565656;
+    border-radius: 10px;
+    padding: 30px;
+  }
 `
 
-export const Label = styled.p``
+export const Label = styled.p`
+  font-size: 14px;
+  color: #ffffff;
+  margin-bottom: 3px;
+`
 
-export const Input = styled.input``
+export const Input = styled.input`
+  height: 40px;
+  border-radius: 8px;
+  background-color: #ffffff;
+  border: none;
+  margin-bottom: 25px;
+  width: 100%;
+  min-width: 280px;
+`
+
+export const ButtonStyle = styled(Button)`
+  width: 100%;
+  min-width: 250px;
+  margin-top: 25px;
+  align-self: center;
+`
+
+export const LabelUpload = styled.label`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border: 1px dashed #ffffff;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 25px;
+  gap: 10px;
+
+  input {
+    opacity: 0;
+    width: 1px;
+  }
+`
